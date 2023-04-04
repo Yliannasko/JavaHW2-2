88bfe56f35f17cc2bfaec21b0edf94605657a0aa
Yulia <ylianna_sko@mail.ru>
2023-04-04 11:16:35 +0300

    three

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int initial = 100; // Сколько средств на счете у клиента
-        int sum = 1500;// Сколько средств клиент внес на счет
+        int sum = 1100;// Сколько средств клиент внес на счет
         int bonus = sum / 100; // 1 бонус = 100 рублей, если клиент пополняет счет более чем на 1000 рублей
         if (sum <= 1000) {
             bonus = 0;
         }
-            System.out.println("Счет клиента с учетом начисленных бонусов составляет:");
-            System.out.println(initial + sum + bonus);
-        }
+        System.out.println("Счет клиента с учетом начисленных бонусов составляет:");
+        System.out.println(initial + sum + bonus);
+        System.out.println("Из них начисленно бонусов:");
+        System.out.println(sum = bonus);
     }
+}
